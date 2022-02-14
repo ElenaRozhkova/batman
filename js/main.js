3c959ec2bdd29f22481e945a38479adce003d4e5
Elena Rozhkova <elenakuzmina88@yandex.ru>
2022-02-14 11:51:05 +0100

    feat: Tag 4

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
 const swiper = new Swiper('.main-slider', {
-    slidesPerView: 2,
     spaceBetween: 30,
     loop: true,
     navigation: {
       nextEl: '.main-slider__arrow',
     },
-  
+    breakpoints: {
+      320 :{
+        slidesPerView: 1,
+      },
+      768 : {
+        slidesPerView: 2,
+      }
+    }
   });
+
+  const burger = document.querySelector('.menu-burger');
+  const closeMenu = document.querySelector('.header__menu-close');
+  const menu = document.querySelector('.header');
+  
+
+  burger.addEventListener('click', ()=>{
+    menu.style.display='block';
+  })
+
+  closeMenu.addEventListener('click', ()=>{
+    menu.style.display='none';
+  })
+
+
   
